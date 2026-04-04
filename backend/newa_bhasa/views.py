66401a4d2030fac83575cf51e159886f866fd74e
oidex69 <shresthaprince222@gmail.com>
2026-04-04 11:17:23 +0545

    backend

--- backend/newa_bhasa/views.py
+++ backend/newa_bhasa/views.py
@@ -5,8 +5,36 @@ import os
 from django.http import JsonResponse
 
 # Create your views here.
-def get_values(request, category, level):
-    file_path = os.path.join(settings.BASE_DIR, 'newa_bhasa', 'data.json')
+def get_picture(request, category, level):
+    file_path = os.path.join(settings.BASE_DIR, 'newa_bhasa', 'Newari_Dataset/data.json')
+
+    with open(file_path, encoding='utf-8') as f:
+        data = json.load(f)
+
+    # filter data
+    filtered = [
+        item for item in data
+        if item['category'].lower() == category.lower() and str(item['level']) == level
+    ]
+
+    return JsonResponse(filtered, safe=False)
+
+def get_audio(request, category, level):
+    file_path = os.path.join(settings.BASE_DIR, 'newa_bhasa', 'Newari_Dataset/data.json')
+
+    with open(file_path, encoding='utf-8') as f:
+        data = json.load(f)
+
+    # filter data
+    filtered = [
+        item for item in data
+        if item['category'].lower() == category.lower() and str(item['level']) == level
+    ]
+
+    return JsonResponse(filtered, safe=False)
+
+def get_text(request, category, level):
+    file_path = os.path.join(settings.BASE_DIR, 'newa_bhasa', 'Newari_Dataset/data.json')
 
     with open(file_path, encoding='utf-8') as f:
         data = json.load(f)
